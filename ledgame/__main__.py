781fd475b7797d06c4a65bcb88a7201cf76cc500
Kent <kentsd16@student.uia.no>
2019-01-20 03:14:28 +0100

    Add low-light toggle

--- ledgame/__main__.py
+++ ledgame/__main__.py
@@ -30,6 +30,8 @@ while True:
                 char.move(0, -1, world)
             elif event.direction == "down":
                 char.move(0, 1, world)
+            elif event.direction == "middle":
+                sense.low_light = !sense.low_light
             view = world.view(char)
             sense.set_pixels(view)
             sense.set_pixel(3, 4, char.color)
