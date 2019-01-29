e95a84b05e6d9e62cd38d5507a866c8c0e667255
KodeGeniElias <KodeGeniElias@users.noreply.github.com>
2019-01-29 12:05:20 +0100

    Implements issue #17

--- ledgame/__main__.py
+++ ledgame/__main__.py
@@ -11,7 +11,7 @@ from os import path
 import time
 
 last_moved = time.time()
-worlds = ["pokepuzzle.png", "tiny.png", "elias.png"]
+worlds = ["teleporttest.png", "tiny.png", "elias.png"]
 world_number = 0
 
 def get_level(filename):
