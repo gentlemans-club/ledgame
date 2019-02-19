a631f4d52a0f6b190e6046a02ea34079bdbadbcf
Yaguel <yaguev17@hotmail.com>
2019-02-19 10:23:43 +0100

    Update __main__.py

--- ledgame/__main__.py
+++ ledgame/__main__.py
@@ -79,7 +79,7 @@ while True:
                 reset_held = False
     if reset_held and time.time() - reset_timer > 1.5: # released after 1.5 seconds
                 world = set_level(world_number, char)
-                print("Level has been reloaled")
+                print("Level has been reloaded")
                 draw(char)
     if any(list(char.moving.values())):
         for direction, value in char.moving.items():
